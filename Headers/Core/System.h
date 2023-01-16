213845a9ea42b5e7f38abe32e2db28ff2d425151
Kevin Chen <chenty0704@gmail.com>
2023-01-16 18:29:45 -0500

    Initial commit.

---
+++ Headers/Core/System.h
@@ -0,0 +1,112 @@
+#pragma once
+
+// Includes concepts library.
+#include <concepts>
+
+// Includes coroutine library.
+#include <coroutine>
+
+// Includes utilities libraries.
+#include <any>
+#include <bitset>
+#include <chrono>
+#include <compare>
+#include <expected>
+#include <functional>
+#include <initializer_list>
+#include <optional>
+#include <source_location>
+#include <tuple>
+#include <type_traits>
+#include <typeindex>
+#include <typeinfo>
+#include <utility>
+#include <variant>
+#include <version>
+
+// Includes dynamic memory management libraries.
+#include <memory>
+#include <memory_resource>
+#include <new>
+#include <scoped_allocator>
+
+// Includes numeric limits library.
+#include <limits>
+
+// Includes error handling libraries.
+#include <cassert>
+#include <exception>
+#include <stacktrace>
+#include <stdexcept>
+#include <system_error>
+
+// Includes string libraries.
+#include <charconv>
+#include <format>
+#include <string>
+#include <string_view>
+
+// Includes container libraries.
+#include <array>
+#include <deque>
+#include <forward_list>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <span>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+// Includes iterator library.
+#include <iterator>
+
+// Includes ranges library.
+#include <ranges>
+
+// Includes algorithm libraries.
+#include <algorithm>
+#include <execution>
+
+// Includes numeric libraries.
+#include <bit>
+#include <complex>
+#include <numbers>
+#include <numeric>
+#include <random>
+#include <ratio>
+
+// Includes I/O libraries.
+#include <fstream>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <spanstream>
+#include <sstream>
+#include <streambuf>
+#include <syncstream>
+
+// Includes filesystem library.
+#include <filesystem>
+
+// Includes regular expression library.
+#include <regex>
+
+// Includes atomic library.
+#include <atomic>
+
+// Includes thread support libraries.
+#include <barrier>
+#include <condition_variable>
+#include <future>
+#include <latch>
+#include <mutex>
+#include <semaphore>
+#include <shared_mutex>
+#include <stop_token>
+#include <thread>
+
+using namespace std;
+using namespace literals;
---
+++ Tests/Core/SystemTest.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(SystemTest, PrecompiledHeader) {
+    const vector vec = {1, 2, 3};
+}
